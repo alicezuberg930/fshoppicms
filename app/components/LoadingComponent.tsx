dc708fa73ccf77054130645707c3df650f84101f
rem <tien23851@gmail.com>
2024-12-07 14:18:54 +0700

    update loading component

--- app/components/LoadingComponent.tsx
+++ app/components/LoadingComponent.tsx
@@ -1,19 +1,26 @@
-const LoadingComponent = () => {
+const LoadingComponent: React.FC<{ isTet?: boolean }> = ({ isTet = true }) => {
     return (
-        <div className="animate-pulse">
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-            <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
-        </div>
+        <>
+            {isTet ?
+                <div className="w-full">
+                    <img src="/assets/tet-wallpaper.jpg" className="w-full" />
+                </div> :
+                <div className="animate-pulse" >
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                    <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700"></div>
+                </div >
+            }
+        </>
     )
 }
 
